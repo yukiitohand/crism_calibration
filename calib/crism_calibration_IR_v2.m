82cc730e455fb7d0e2278f7b33c9021aa58f080f
Yuki Itoh <yukiitohand@gmail.com>
2019-05-09 13:02:28 -0400

    code gets simplified with external functions.

--- calib/crism_calibration_IR_v2.m
+++ calib/crism_calibration_IR_v2.m
@@ -102,55 +102,17 @@ crism_obs = CRISMObservation(obs_id,'SENSOR_ID','L');
 % crism_obsS = CRISMObservation(obs_id,'SENSOR_ID','S');
 switch upper(crism_obs.info.obs_classType)
     case {'FRT','HRL','HRS','FRS','ATO','MSP','HSP'}
-        if ~isempty(crism_obs.info.basenameIF)
-            TRRIF_is_empty = false;
-            TRRIFdata = CRISMdata(crism_obs.info.basenameIF,'');
-            % TRRIFdataS = CRISMdata(crism_obsS.info.basenameIF,'');
-            TRRRAdata = CRISMdata(crism_obs.info.basenameRA,'');
-        elseif ~isempty(crism_obs.info.basenameRA)
-            TRRIF_is_empty = true;
-            TRRIFdata = CRISMdata(crism_obs.info.basenameRA,'');
-            % TRRIFdataS = CRISMdata(crism_obsS.info.basenameRA,'');
-            TRRRAdata = CRISMdata(crism_obs.info.basenameRA,'');
-        else
-            error('Check data');
-        end
-        DDRdata = CRISMdata(crism_obs.info.basenameDDR,'');
+        TRRIFdata = get_CRISMdata(crism_obs.info.basenameIF,'');
+        TRRRAdata = get_CRISMdata(crism_obs.info.basenameRA,'');
+        DDRdata = get_CRISMdata(crism_obs.info.basenameDDR,'');
     case {'FFC'}
-        switch ffc_counter
-            case 1
-                if ~isempty(crism_obs.info.basenameIF)
-                    TRRIF_is_empty = false;
-                    TRRIFdata = CRISMdata(crism_obs.info.basenameIF{1},'');
-                    % TRRIFdataS = CRISMdata(crism_obsS.info.basenameIF{1},'');
-                    TRRRAdata = CRISMdata(crism_obs.info.basenameRA{1},'');
-                elseif ~isempty(crism_obs.info.basenameRA)
-                    TRRIF_is_empty = true;
-                    TRRIFdata = CRISMdata(crism_obs.info.basenameRA{1},'');
-                    % TRRIFdataS = CRISMdata(crism_obsS.info.basenameRA{1},'');
-                    TRRRAdata = CRISMdata(crism_obs.info.basenameRA{1},'');
-                else
-                    error('Check data');
-                end
-                DDRdata = CRISMdata(crism_obs.info.basenameDDR{1},'');
-            case 3
-                if ~isempty(crism_obs.info.basenameIF)
-                    TRRIF_is_empty = false;
-                    TRRIFdata = CRISMdata(crism_obs.info.basenameIF{2},'');
-                    % TRRIFdataS = CRISMdata(crism_obsS.info.basenameIF{2},'');
-                    TRRRAdata = CRISMdata(crism_obs.info.basenameRA{2},'');
-                elseif ~isempty(crism_obs.info.basenameRA)
-                    TRRIF_is_empty = true;
-                    TRRIFdata = CRISMdata(crism_obs.info.basenameRA{2},'');
-                    % TRRIFdataS = CRISMdata(crism_obsS.info.basenameRA{2},'');
-                    TRRRAdata = CRISMdata(crism_obs.info.basenameRA{2},'');
-                else
-                    error('Check data');
-                end
-                DDRdata = CRISMdata(crism_obs.info.basenameDDR{2},'');
-            otherwise
-                error('Check data');
-        end
+        [TRRIFdata] = get_scene_CRISMdata_FFC(crism_obs.info.basenameIF,'',ffc_counter);
+        [TRRRAdata] = get_scene_CRISMdata_FFC(crism_obs.info.basenameRA,'',ffc_counter);
+        [DDRdata] = get_scene_CRISMdata_FFC(crism_obs.info.basenameDDR,'',ffc_counter);
+end
+TRRIF_is_empty = isempty(TRRIFdata);
+if TRRIF_is_empty
+    TRRIFdata = TRRRAdata;
 end
 [DFdata1,DFdata2] = get_DFdata4SC(TRRIFdata,crism_obs);
 %% setting saving directory
@@ -160,7 +122,7 @@ else
     save_dir = joinPath(save_pdir,crism_obs.info.dirname);
 end
 
-propIF = getProp_basenameOBSERVATION(TRRIFdata.basename);
+propIF = TRRIFdata.prop;
 propIF.product_type = product_type;
 propIF.version = vr;
 if TRRIF_is_empty
@@ -272,7 +234,7 @@ switch lower(mode_calib)
         %custom SPdata
         bkoption = 2;
         [RDn,RDn_woc,RDn_bk1_o,RDn_bk2_o] = pipeline_calibration_IR_IF_wTRRIF_yuki(...
-        TRRIFdata,TRRIFdataS,crism_obs,bkoption,...
+        TRRIFdata,[],crism_obs,bkoption,...
         'SAVE_MEMORY',save_mem,'dwld',2,'MODE_SP','MAN',...
         'APBPRMVL','HighOrd','SATURATION_RMVL',2,...
         'SP_SATURATION_RMVL',2,'SP_APBPRMVL','HighOrd','SP_MEAN_ROBUST',1,'SP_MEAN_DN14',1,...
